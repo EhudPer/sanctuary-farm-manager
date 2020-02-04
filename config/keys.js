bb08e751e6fa1218be45c3663883698b78419a44
Ehud Perlman <ehudperlman83@gmail.com>
2020-02-04 12:35:54 +0200

    changed mongodbURI string

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongodbURI:
-    'mongodb+srv://dbAdminUser:Ehud1983@sanctuary-farm-db-ivegc.mongodb.net/sanctuary-farm-db?retryWrites=true&w=majority'
+    'mongodb+srv://dbAdminUser:admin@sanctuary-farm-db-ivegc.mongodb.net/sanctuary-farm-db?retryWrites=true&w=majority'
 }
