7dd8fb8ac54e0afcc3d871048442f0d8e53b7386
Ehud Perlman <ehudperlman83@gmail.com>
2020-02-04 13:24:18 +0200

    added path to serverjs

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const animals = require('./routes/api/animals')
 
