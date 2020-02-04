fb6fec28259189f5c666498de5a172040359b029
Ehud Perlman <ehudperlman83@gmail.com>
2020-02-04 14:05:47 +0200

    .

--- server.js
+++ server.js
@@ -44,9 +44,9 @@ if (process.env.NODE_ENV === 'production') {
     else res.redirect(`https://'${req.headers.host}${req.url}`)
   })
 
-  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.use(express.static(path.join(__dirname + '/client/build')))
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + 'client/build/index.html'))
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
   })
 }
