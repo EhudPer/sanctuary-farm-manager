e0d4a3b4d45118901c89194a0cb4790203804bde
Ehud Perlman <ehudperlman83@gmail.com>
2020-02-10 15:55:42 +0200

    minor add release notes

--- client/src/files/releaseNotes.js
+++ client/src/files/releaseNotes.js
@@ -1,3 +1,3 @@
-const releaseNotes = `Version 1.0.1:\n* - Added registration and login functionality and pages.\n* - Added home page.\n* - Animals are per user only.\n* - added some general styling.`
+const releaseNotes = `Version 1.0.1:\n* - Added registration and login functionality and pages.\n* - Added home page.\n* - Animals are per user only.\n* - added some general styling.\n* = Added delete animal support.`
 
 export default releaseNotes
