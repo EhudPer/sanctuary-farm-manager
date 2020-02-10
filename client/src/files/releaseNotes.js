46dce7c079623da65f9e61d4e6dc249e88aaa397
Ehud Perlman <ehudperlman83@gmail.com>
2020-02-10 17:11:33 +0200

    added author

--- client/src/files/releaseNotes.js
+++ client/src/files/releaseNotes.js
@@ -1,3 +1,3 @@
-const releaseNotes = `Version 1.0.1:\n* - Added registration and login functionality and pages.\n* - Added home page.\n* - Animals are per user only.\n* - added some general styling.\n* = Added delete animal support.`
+const releaseNotes = `Author: Ehud Perlman\nVersion 1.0.1:\n* - Added registration and login functionality and pages.\n* - Added home page.\n* - Animals are per user only.\n* - added some general styling.\n* = Added delete animal support.`
 
 export default releaseNotes
